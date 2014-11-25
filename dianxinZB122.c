adae5fc467fcee20b727d97fbef2841d34237d75
limanyi <545387015@qq.com>
2014-11-25 22:09:29 +0800

    add hello.c dianxinZB122.c

---
+++ dianxinZB122.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+void main()
+{
+
+
+void dian();
+void xin();
+void Z();
+void B();
+void yi();
+void er();
+void xiaolian();
+dian();
+xin();
+Z();
+B();
+yi();
+er();
+er();
+xiaolian();
+printf("Enter:");
+scanf("Enter a  key end:");
+
+}
+void dian()
+{
+printf("    **   \n");
+printf("**********\n");
+printf("**  **  **\n");
+printf("**  **  **\n");
+printf("**********  **\n");
+printf("    **********\n");
+printf("//**************************************//\n");
+printf("//**************************************//\n");
+}
+void xin()
+{
+printf("   **   **\n");
+printf("  **  ******\n");
+printf(" **    ****\n");
+printf("* *   ******\n");
+printf("  *   ******\n");
+printf("  *   **  **\n");
+printf("  *   ******\n");
+printf("//**************************************//\n");
+printf("//**************************************//\n");
+}
+void Z()
+{
+printf(" ************\n");
+printf("          **\n");
+printf("         **\n");
+printf("       **\n");
+printf("     **\n");
+printf("   **\n");
+printf(" ************\n");
+printf("//**************************************//\n");
+printf("//**************************************//\n");
+}
+void B()
+{
+printf(" *********\n");
+printf(" **       *\n");
+printf(" **      **\n");
+printf(" **     **\n");
+printf(" **  **\n");
+printf(" **     **\n");
+printf(" **      **\n");
+printf(" **       *\n");
+printf(" *********\n");
+printf("//**************************************//\n");
+printf("//**************************************//\n");
+}
+void yi()
+{
+printf("    ***\n");
+printf("   ****\n");
+printf("  *****\n");
+printf("     **\n");
+printf("     **\n");
+printf("     **\n");
+printf("     **\n");
+printf("     **\n");
+printf("  ********\n");
+printf("//**************************************//\n");
+printf("//**************************************//\n");
+}
+void er()
+{
+printf("    ****\n");
+printf("  **    **\n");
+printf(" **      **\n");
+printf("         **\n");
+printf("       **\n");
+printf("    **\n ");
+printf("  **\n");
+printf(" **********\n");
+printf("//**************************************//\n");
+printf("//**************************************//\n");
+}
+void xiaolian()
+{
+printf("  **    **\n");
+printf(" *  *  *  *\n");
+printf("     **\n");
+printf("   *    *\n");
+printf("     **\n");
+printf("//**************************************//\n");
+printf("//**************************************//\n");
+}
